a47ad293c85ff064d1dcf804b66b8975b5d3df5d
AlexandruGirlea01 <alexandrugirlea36@gmail.com>
2024-01-14 11:59:27 +0200

    Fixed first page + added routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,36 @@ import Page2 from './second_page/second_page';
 import Page3 from './girafa/girafa';
 import Page4 from './ghepard/ghepard';
 import Page1 from './first_page/first_page';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+
 export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Page1 />,
+    },
+    {
+      path: "/page2",
+      element: <Page2 />,
+    },
+    {
+      path: "/page3",
+      element: <Page3 />,
+    },
+    {
+      path: "/page4",
+      element: <Page4 />,
+    },
+    {
+      path: "/page5",
+      element: <Page5 />,
+    },
+  ])
+
   return (
-    <div>
-      <Page1 />
-    </div>
-  );
+      <RouterProvider router={router} />
+  )
 }
